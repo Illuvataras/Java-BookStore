40ddc4c2e41bb0b6e8e19b3799a1fad557657ff6
Vytautas Ambrasas <chatoman@gmail.com>
2019-05-21 12:42:53 +0300

    Completed the module

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -101,4 +101,19 @@ public class BookDAO {
             e.printStackTrace();
         }
     }
+    public void updateBook(Book book){
+        String SQLU = "UPDATE book SET title = ?, author = ?, price = ? WHERE id = ?";
+
+        try{
+            PreparedStatement statement = jdbcConnection.prepareStatement(SQLU);
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getAuthor());
+            statement.setFloat(3, book.getPrice());
+            statement.setInt(4, book.getId());
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
